976a524348f5be7d00eef8bd2c94e12119d46ba4
BLVCKx <wael.hcine@esprit.tn>
2023-10-23 15:18:17 +0100

    initialisation

--- src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
+++ src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class GestionStationSkiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GestionStationSkiApplication.class, args);
 	}
