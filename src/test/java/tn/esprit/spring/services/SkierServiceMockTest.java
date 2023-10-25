e55e30db29c0de7e005e6ef626d3f115c8cf721b
BLVCKx <wael.hcine@esprit.tn>
2023-10-25 13:27:47 +0100

    update file test

--- src/test/java/tn/esprit/spring/services/SkierServiceMockTest.java
+++ src/test/java/tn/esprit/spring/services/SkierServiceMockTest.java
@@ -9,38 +9,65 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.spring.entities.Piste;
-import tn.esprit.spring.entities.Registration;
-import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.entities.*;
 import tn.esprit.spring.repositories.ISkierRepository;
 
 import java.time.LocalDate;
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.verify;
+
+
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @Slf4j
+
 public class SkierServiceMockTest  {
     @Mock
-    ISkierRepository skierRepository;
+    ISkierRepository skierRepo;
+    //ou ISkierRepository skierRepo = Mockito.mock(ISkierRepository.class);
+
     @InjectMocks
-    SkierServicesImpl skierServices;
+    SkierServicesImpl skierService;
 
-    Set<Piste> pistes = new HashSet<>();
-    Set<Registration> registrations = new HashSet<>();
-
-    Skier skier = new Skier( 1L,"wael","hcine", LocalDate.of(2018, 5, 5),"ariana", pistes,registrations);
-    List<Skier> skierList = new ArrayList<Skier>() {
+    Subscription s=Subscription.builder().price(200F).endDate(LocalDate.parse("2025-12-04"))
+            .numSub(2L).typeSub(TypeSubscription.ANNUAL).startDate(LocalDate.parse("2024-12-04"))
+            .build();
+    Skier m  = Skier.builder().numSkier(3L).dateOfBirth(LocalDate.ofEpochDay(1999-12-19))
+            .city("Tunis").firstName("wael").lastName("hcine").subscription(s).build();
+    List<Skier> list= new ArrayList<Skier>() {
         {
-            add(new Skier(12L,"wael1","hcine1", LocalDate.of(2019, 6, 6),"ariana1", pistes,registrations));
-            add(new Skier(13L,"wael2","hcine2", LocalDate.of(2022, 7, 7),"ariana2", pistes,registrations));
+            add(Skier.builder().numSkier(2L).dateOfBirth(LocalDate.ofEpochDay(1999-11-19))
+                    .city("Tunis").firstName("ahmed").lastName("hfaiedh").build());
+            add(Skier.builder().numSkier(75L).dateOfBirth(LocalDate.ofEpochDay(2000-10-18))
+                    .city("Tunis").firstName("saif").lastName("amara").build());
         }
     };
+
     @Test
-    public void retrieveSkier (){
-        Mockito.when(skierRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(skier));
-        Skier skier1 = skierServices.retrieveSkier(Long.valueOf("1L"));
-        Assertions.assertNotNull(skier1);
+    public void addSkierTest() {
+        Mockito.when(skierRepo.save(Mockito.any(Skier.class))).then(invocation -> {
+            Skier model = invocation.getArgument(0, Skier.class);
+            model.setNumSkier(1L);
+            return model;
+        });
+        log.info("Avant ==> " + m.toString());
+        Skier skier = skierService.addSkier(m);
+        assertSame(skier, m);
+        log.info("Après ==> " + m.toString());
     }
+
+    @Test
+    public void retreiveSkierTest() {
+        Mockito.when(skierRepo.findById(Mockito.anyLong())).thenReturn(Optional.of(m));
+        Skier skier = skierService.retrieveSkier((long) 75);
+        assertNotNull(skier);
+        log.info("get ==> " + skier.toString());
+        verify(skierRepo).findById(Mockito.anyLong());
+
+    }
+
 
 }
