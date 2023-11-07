fc89b8313280af6c2c942508cfda7c733ba0745f
SassiWala <99282536+SassiWala@users.noreply.github.com>
2023-11-07 11:03:22 +0100

    Update PisteRestController.java

--- src/main/java/tn/esprit/spring/controllers/PisteRestController.java
+++ src/main/java/tn/esprit/spring/controllers/PisteRestController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/piste")
 @RequiredArgsConstructor
+@CrossOrigin
 public class PisteRestController {
 
     private final IPisteServices pisteServices;
