a4f9f7f667e267f656c6431e6d28027ce3052af9
amineBouzouita <amine.bouzouita@esprit.tn>
2023-11-13 15:02:45 +0100

    jenkis file

--- src/test/java/tn/esprit/spring/service/RegistrationServiceMockTest.java
+++ src/test/java/tn/esprit/spring/service/RegistrationServiceMockTest.java
@@ -37,5 +37,86 @@ class RegistrationServiceMockTest {
     @InjectMocks
     private RegistrationServicesImpl registrationServices;
 
+    @Test
+    void testAddRegistrationAndAssignToSkier() {
+        // Mock data
+        Registration registration = new Registration();
+        Long numSkier = 1L;
+        Skier skier = new Skier();
 
+
+        // Mock the behavior of the skierRepository
+        when(skierRepository.findById(numSkier)).thenReturn(Optional.of(skier));
+
+        // Mock the behavior of the registrationRepository
+        when(registrationRepository.save(any())).thenReturn(registration);
+
+        // Test the service method
+        Registration result = registrationServices.addRegistrationAndAssignToSkier(registration, numSkier);
+
+        // Verify that skierRepository.findById was called once with the correct argument
+        verify(skierRepository, times(1)).findById(numSkier);
+
+        // Verify that registrationRepository.save was called once with the correct argument
+        verify(registrationRepository, times(1)).save(any());
+
+        // Assert the result
+        assertEquals(registration, result);
+    }
+
+    @Test
+    void testAssignRegistrationToCourse() {
+        // Mock data
+        Long numRegistration = 1L;
+        Long numCourse = 2L;
+        Registration registration = new Registration();
+
+        Course course = new Course();
+
+
+        // Mock the behavior of the registrationRepository
+        when(registrationRepository.findById(numRegistration)).thenReturn(Optional.of(registration));
+
+        // Mock the behavior of the courseRepository
+        when(courseRepository.findById(numCourse)).thenReturn(Optional.of(course));
+
+        // Mock the behavior of the registrationRepository.save
+        when(registrationRepository.save(any())).thenReturn(registration);
+
+        // Test the service method
+        Registration result = registrationServices.assignRegistrationToCourse(numRegistration, numCourse);
+
+        // Verify that registrationRepository.findById was called once with the correct argument
+        verify(registrationRepository, times(1)).findById(numRegistration);
+
+        // Verify that courseRepository.findById was called once with the correct argument
+        verify(courseRepository, times(1)).findById(numCourse);
+
+        // Verify that registrationRepository.save was called once with the correct argument
+        verify(registrationRepository, times(1)).save(any());
+
+        // Assert the result
+        assertEquals(registration, result);
+    }
+
+
+
+    @Test
+    void testNumWeeksCourseOfInstructorBySupport() {
+        // Mock data
+        Long numInstructor = 1L;
+        Support support = Support.SKI; // Mock the enum correctly
+
+        // Mock the behavior of the registrationRepository
+        when(registrationRepository.numWeeksCourseOfInstructorBySupport(numInstructor, support)).thenReturn(Arrays.asList(1, 2, 3));
+
+        // Test the service method
+        List<Integer> result = registrationServices.numWeeksCourseOfInstructorBySupport(numInstructor, support);
+
+        // Verify that registrationRepository.numWeeksCourseOfInstructorBySupport was called once with the correct arguments
+        verify(registrationRepository, times(1)).numWeeksCourseOfInstructorBySupport(numInstructor, support);
+
+        // Assert the result
+        assertEquals(Arrays.asList(1, 2, 3), result);
+    }
 }
